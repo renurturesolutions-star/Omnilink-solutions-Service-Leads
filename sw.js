c8187e17d0c420c64482b9577efa902d67241c8e
renurturesolutions-star <renurture.solutions@gmail.com>
2025-11-10 05:45:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('omni-v1').then(c=>c.addAll(['./','./index.html','./contractor.html','./addons.js'])));
+  e.waitUntil(caches.open('omni-v1').then(c=>c.addAll(['./','./index.html','./contractor.html','./addons_master.js'])));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
